6a5cf033b42cc646ba6ad6d0d1f023514045ae94
Prashanth Aitha <68016207+prashanth624@users.noreply.github.com>
2023-02-26 13:38:51 -0600

    Update LoginTest.java

--- src/test/java/testCases/LoginTest.java
+++ src/test/java/testCases/LoginTest.java
@@ -10,5 +10,6 @@ public class LoginTest {
 	public void LoginTestClass(String city) {
 		System.out.println("Name of the city is: "+city);
 		System.out.println("this is login test class");
+		System.out.println("new comment after ngrok setup for automatic build in jenkins");
 	}
 }
