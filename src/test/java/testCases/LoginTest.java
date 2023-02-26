85c5c2ced1add82463c7a747b238bb10a0ffb9e4
Prashanth Aitha <aprashanth246@gmail.com>
2023-02-25 18:39:58 -0600

    updated parameters

--- src/test/java/testCases/LoginTest.java
+++ src/test/java/testCases/LoginTest.java
@@ -1,12 +1,14 @@
 package testCases;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
-	
+	@Parameters("CityName")
 	@Test
-	public void LoginTestClass() {
+	public void LoginTestClass(String city) {
+		System.out.println("Name of the city is: "+city);
 		System.out.println("this is login test class");
 	}
 }
