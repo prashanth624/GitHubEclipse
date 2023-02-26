600917b94b91e8781e4b0030298e0090590c167b
Prashanth Aitha <aprashanth246@gmail.com>
2023-02-25 18:07:34 -0600

    first commit

---
+++ src/test/java/testCases/LoginTest.java
@@ -0,0 +1,12 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	
+	@Test
+	public void LoginTestClass() {
+		System.out.println("this is login test class");
+	}
+}
